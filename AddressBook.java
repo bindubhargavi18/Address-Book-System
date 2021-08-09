44be0511bf57907a9fc1463af391731f92b2ee70
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-10 01:04:34 +0530

    Added:Edit the person details by name

--- AddressBook.java
+++ AddressBook.java
@@ -43,6 +43,44 @@ class AddressBook
 								+info.state+" "+info.email+" "+info.zip+" "+info.phn);
 		}
 	}
+	void editContact()
+	{
+		firstName=sc.next();
+		int flag=0;
+		for(int i=0;i<contact.size();i++)
+		{
+			info=contact.get(i);
+			if(firstName.equals(info.firstName))
+			{
+					System.out.println("Enter your new address:");
+					address=sc.next();
+					System.out.println("Enter your new city:");
+					city=sc.next();
+					System.out.println("Enter your new state:");
+					state=sc.next();
+					System.out.println("Enter your new email:");
+					email=sc.next();
+					System.out.println("Enter your new zip:");
+					zip=sc.nextInt();
+					System.out.println("Enter your new phn number:");
+					phn=sc.nextLong();
+					contact.set(i,new Contacts(firstName,lastName,address,city,state,zip,phn,email));
+					flag=1;
+			}
+		}
+		if(flag==0)
+			System.out.println("Given name is not found in address book..");
+		System.out.println();
+		System.out.println("Address book after editing details:");
+		for(int i=0;i<number;i++)
+		{
+			info=contact.get(i);
+			System.out.println();
+			System.out.println(info.firstName+" "+info.lastName+" "+info.address+" "+info.city+" "
+								+info.state+" "+info.email+" "+info.zip+" "+info.phn);
+		}
+
+	}
 }
 	
 
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,7 +5,17 @@ class AddressBookMain
 	{
 		System.out.println("Enter to address book system");	
 		AddressBook book=new AddressBook();
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter how many contacts do u want to add");
 		book.addContact();
+
+		System.out.println("Do you want to edit the contact? yes or no ");
+		String option=sc.next();
+		if(option.equals("yes"))
+		{
+			System.out.println();
+			System.out.println("Enter first name to edit:");
+			book.editContact();
+		}
 	}
 }
